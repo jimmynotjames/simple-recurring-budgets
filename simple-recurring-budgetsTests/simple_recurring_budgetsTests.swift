372c23fd9e53e6b64ed38309db7b234e26afc13c
Jimmy Ho <jimmynotjames@mac.com>
2026-04-10 10:51:43 -0400

    Initial Commit

---
+++ simple-recurring-budgetsTests/simple_recurring_budgetsTests.swift
@@ -0,0 +1,19 @@
+//
+//  simple_recurring_budgetsTests.swift
+//  simple-recurring-budgetsTests
+//
+//  Created by Jimmy Ho on 4/10/26.
+//
+
+import Testing
+@testable import simple_recurring_budgets
+
+struct simple_recurring_budgetsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
